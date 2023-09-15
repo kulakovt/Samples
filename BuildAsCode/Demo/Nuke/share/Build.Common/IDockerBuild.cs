6b66317e4f22a04b024bf4e990f1239fc40e7b28
Anatoly Kulakov <anatoly.kulakov@outlook.com>
2023-09-15 23:58:14 +0300

    Add nuke build sample to buldac

---
+++ BuildAsCode/Demo/Nuke/share/Build.Common/IDockerBuild.cs
@@ -0,0 +1,67 @@
+﻿using Build.Common.Integration;
+using Nuke.Common;
+using Serilog;
+using static Nuke.Common.Tools.Docker.DockerTasks;
+
+namespace Build.Common;
+
+[ParameterPrefix(nameof(Docker))]
+public interface IDockerBuild : IBaseBuild
+{
+    const string DockerContainerArtifactsPath = "/app/artifacts";
+
+    string DockerImageName => ServiceName.ToKebabCaseLower();
+
+    string DockerfilePath => RootDirectory / "build" / "Dockerfile";
+    
+    [Parameter("Docker repositories url"), Required]
+    Uri RepositoriesUrl => this.GetValue(() => RepositoriesUrl);
+
+    [Parameter("Docker project name"), Required]
+    string ProjectName => this.GetValue(() => ProjectName);
+
+    string ResolveDockerImageNameTag()
+    {
+        var version = this.ResolveVersion();
+        return $"{RepositoriesUrl.Authority}/{ProjectName}/{DockerImageName}:{version.Version}";
+    }
+
+    /// <summary>
+    /// Dockerfile processing pipeline: build -> create container -> copy artifacts -> remove container
+    /// </summary>
+    Target BuildDockerfile => _ => _
+        .Requires(() => RepositoriesUrl)
+        .Requires(() => ProjectName)
+        .Executes(() =>
+        {
+            SetupLogging();
+
+            this.BuildDocker();
+
+            var containerId = this.CreateDockerContainer();
+            this.CopyArtifactsFromContainer(containerId);
+
+            this.RemoveDockerContainer(containerId);
+        });
+
+    /// <summary>
+    /// Push Docker image to the repository
+    /// </summary>
+    Target PushDockerArtifacts => _ => _
+        .Requires(() => RepositoriesUrl)
+        .Requires(() => ProjectName)
+        .TryDependsOn<IDockerBuild>(x => x.BuildDockerfile)
+        .Executes(() =>
+        {
+            SetupLogging();
+
+            var imageTag = ResolveDockerImageNameTag();
+            // DockerPush(settings => settings
+            //     .SetName(imageTag);
+            Log.Information("Docker image {DockerImageName} was pushed to {DockerImageTag}", DockerImageName, imageTag);
+        });
+
+    // Workaround for logging issue in Nuke with Docker tasks
+    // See more details here: https://nuke.build/faq
+    static void SetupLogging() => DockerLogger = (_, text) => Log.Debug(text);
+}
---
+++ BuildAsCode/Demo/Nuke/share/Build.Common/INuGetBuild.cs
@@ -0,0 +1,46 @@
+﻿using Build.Common.Integration;
+using Nuke.Common;
+using Nuke.Common.IO;
+using Serilog;
+
+namespace Build.Common;
+
+[ParameterPrefix(nameof(NuGet))]
+public interface INuGetBuild: IBaseBuild
+{
+    [Parameter("NuGet url"), Required]
+    Uri Url => this.GetValue(() => Url);
+
+    [Parameter("NuGet feed name"), Required]
+    string FeedName => this.GetValue(() => FeedName);
+
+    //[Secret]
+    [Parameter("NuGet API key"), Required]
+    string ApiKey => this.GetValue(() => ApiKey);
+
+    AbsolutePath NuGetArtifactsPath => ArtifactsPath / "nuget";
+    
+    Target PushNuGetArtifacts => _ => _
+        .Requires(() => Url)
+        .Requires(() => FeedName)
+        .Requires(() => ApiKey)
+        .TryDependsOn<IDockerBuild>(x => x.BuildDockerfile)
+        .Executes(() =>
+        {
+            var nuGetPushUrl = Url.Combine($"nuget/{FeedName}/packages");
+
+            // DotNetNuGetPush(settings =>
+            //     settings
+            //         .SetTargetPath(NuGetArtifactsPath / "*.nupkg")
+            //         .SetSource(nuGetPushUrl.OriginalString)
+            //         .SetApiKey(ApiKey)
+            //         .EnableSkipDuplicate()
+            //         .EnableForceEnglishOutput());
+
+            var pushedArtifacts = NuGetArtifactsPath.GlobFiles("*.nupkg")
+                .Select(x => x.Name)
+                .ToList();
+            
+            Log.Information("Nuget artifacts {NuGetArtifacts} were successfully pushed to {NuGetUrl}", pushedArtifacts, nuGetPushUrl);
+        });
+}
